87cec73494054129690a13a11a856eca530ad86b
CMD SHAKEEL <cmdshakeel21@gmail.com>
2021-08-06 23:05:42 +0530

    Added:-last name validation is added

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -4,9 +4,21 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
+    /*
+      using the pattern for first name
+     */
+
     private static final String FIRST_NAME_REGEX = "^[a-zA-Z]+$";
 
     public boolean validateFirstName(String firstName) {
+
         return Pattern.matches(FIRST_NAME_REGEX, firstName);
+    }
+
+    private static final String SECOND_NAME_REGEX = "^[a-zA-Z]{3,}$";
+
+    public boolean validateSecondName(String  SecondName) {
+
+        return Pattern.matches(SECOND_NAME_REGEX, SecondName);
     }
 }
--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -1,16 +1,20 @@
 package com.bridgelabz;
-
+//importing  junit assert
 import org.junit.Assert;
+//importing j unit Test
 import org.junit.Test;
 
 public class UserRegistrationTest {
 
     @Test
     public void givenValidFirstName_ShouldReturnTrue_Test() {
+        /*
+            creating the object of userRegistrstion
+         */
 
         UserRegistration userRegistration = new UserRegistration();
 
-        boolean result = userRegistration.validateFirstName("Shakeel");
+        boolean result = userRegistration.validateFirstName("CMD");
 
         Assert.assertTrue(result);
     }
@@ -20,8 +24,31 @@ public class UserRegistrationTest {
 
         UserRegistration userRegistration = new UserRegistration();
 
-        boolean result = userRegistration.validateFirstName("shakeel");
+        boolean result = userRegistration.validateFirstName("CMD");
 
         Assert.assertFalse(result);
+
+
+    }
+
+    @Test
+    public void givenValidSecondName_ShouldReturnTrue_Test() {
+
+        UserRegistration userRegistration = new UserRegistration();
+
+        boolean result = userRegistration.validateFirstName("SHAKEEL");
+
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenInValidSecondName_ShouldReturnFalse_Test() {
+
+        UserRegistration userRegistration = new UserRegistration();
+
+        boolean result = userRegistration.validateFirstName("SHAKEEL");
+
+        Assert.assertFalse(result);
+
     }
 }
